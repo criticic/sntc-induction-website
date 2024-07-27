fb304852fe49f45d6bbc296bc5d7fe3c4bf23f1c
Divyansh Seth <divyanshseth08@gmail.com>
2024-07-27 21:32:13 +0530

    created get started page

--- src/routes/signin-page.tsx
+++ src/routes/signin-page.tsx
@@ -2,10 +2,37 @@ import { SignInButton } from "@clerk/clerk-react";
 
 export default function SignInPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-orange-50">
-      <div className="rounded-lg bg-[orange] p-2 font-bold">
-        <SignInButton />
+    <div className="grid min-h-screen auto-rows-min flex-col bg-[#0c0a09] p-16 md:flex md:flex-row md:p-48">
+      <div>
+        <div className="md:ml-8">
+          <h1 className="mb-4 max-w-[40rem] text-center text-5xl font-extrabold leading-none text-[#f2f2f2] sm:text-left md:text-5xl xl:text-6xl">
+            Science and Technology Council
+          </h1>
+          <p className="text-muted-foreground mb-6 max-w-2xl text-center font-light text-[#a1a1aa] sm:text-left md:text-lg lg:mb-8 lg:text-xl">
+            The official technical body of IIT (BHU) Varanasi
+          </p>
+          <button className="hidden h-10 w-36 rounded-md bg-[#22c55e] text-sm font-normal hover:bg-[#22c55ee6] md:block">
+            <SignInButton>
+              <button>Get Started</button>
+            </SignInButton>
+          </button>
+        </div>
       </div>
+      <div className="video grid">
+        <video
+          autoPlay
+          muted
+          loop
+          className="w-full justify-self-center md:w-52"
+        >
+          <source src="/rocket.webm" type="video/webm" />
+        </video>
+      </div>
+      <button className="mt-8 h-12 w-full rounded-md bg-[#22c55e] text-xl font-medium hover:bg-[#22c55ee6] md:hidden">
+        <SignInButton>
+          <button>Get Started</button>
+        </SignInButton>
+      </button>
     </div>
   );
 }
